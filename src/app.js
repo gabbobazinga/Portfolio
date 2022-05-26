50e4d6b7a2936c4b15e2be2899ca68031f2dd457
Gabriele <gabriele.amore@eroisolutions.it>
2022-05-26 14:28:44 +0200

    update website

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+import './app.scss'
 
 new Vue({
     el: '#app',
@@ -11,8 +12,27 @@ new Vue({
                 title: 'Convengo.it',
                 tags: ['HTML', 'SCSS', 'JS', 'VUE', 'LARAVEL', 'PHP'],
                 webUrl: 'https://convengo.it',
+                showWebBtn: true,
                 codeUrl: '',
                 pathImg: 'https://i.ibb.co/mS32WTw/convengo-it.png',
+                info: 'Sistema di comparazione che permette a tutti, privati e aziende, di accedere e confrontare le offerte e i fornitori luce, gas, internet casa e non solo, in modo semplice e veloce, con la possibilità di richiedere una consulenza personalizzata.',
+            },
+            {
+                title: 'Area Utente',
+                tags: ['VUE', 'VUE-ROUTER', 'AXIOS', 'LARAVEL'],
+                webUrl: 'https://areautente.convengo.it',
+                showWebBtn: true,
+                codeUrl: '',
+                pathImg: 'https://i.ibb.co/JCB2XyW/area-utente.png',
+                info: 'Area Utente di Convengo.it, in cui gli utenti possono usufruire dei punti a loro disposizione per ottenere cashback ed acquistare nuovi prodotti.',
+            },
+            {
+                title: 'Amicatariffa.it',
+                tags: ['HTML', 'SCSS', 'JS', 'VUE', 'LARAVEL', 'PHP'],
+                webUrl: 'https://amicatariffa.it',
+                showWebBtn: true,
+                codeUrl: '',
+                pathImg: 'https://i.ibb.co/dkjTgDR/amicatariffa.png',
                 info: 'Sistema di comparazione che permette a tutti, privati e aziende, di accedere e confrontare le offerte e i fornitori luce, gas, internet casa e non solo, in modo semplice e veloce, con la possibilità di richiedere una consulenza personalizzata.',
             },
             {
@@ -21,7 +41,7 @@ new Vue({
                 webUrl: 'https://gabbobazinga.github.io/Boolflix/',
                 codeUrl: 'https://github.com/gabbobazinga/Boolflix',
                 pathImg: 'https://i.ibb.co/Mp8xkDN/boolflix.png',
-                info: 'Riproduzione di un sito simil Netflix con utilizzo di Vue, chiamate delle API del sito "The Movie DB" tramite Axios. Homepage che mostra i film più popolari suddivisi per categoria, possibilità di visione serie tv e film più popolari, implementazione dell "Infinite Scroll" ed al passaggio del mouse sulla card vista nel dettaglio di tutte le informazioni',
+                info: 'Riproduzione di un sito simil Netflix con utilizzo di Vue, chiamate delle API del sito "The Movie DB" tramite Axios. Homepage che mostra i film più popolari suddivisi per categoria, possibilità di visione serie tv e film più popolari, implementazione dell "Infinite Scroll".',
             },
             {
                 title: 'Spotify',
@@ -94,4 +114,38 @@ new Vue({
     mounted() {
         window.addEventListener('scroll', this.handleScroll);
     }
+});
+
+$(document).ready(function(){
+    $('.logos').slick({
+        infinite: true,
+        arrows: false,
+        dots: false,
+        slidesToShow: 8,
+        slidesToScroll: 2,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        centerMode: true,
+        variableWidth: true,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 6,
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 4,
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 4,
+              }
+            }
+          ]
+    });
 });
